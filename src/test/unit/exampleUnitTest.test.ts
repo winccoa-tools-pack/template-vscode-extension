b1167a7084fdbd62e06f9074c670eb1e1f091f01
Martin Pokorny <martin.pokorny@etm.at>
2026-01-13 21:19:31 +0100

    refactor: remove unused imports and variables in example unit test

--- src/test/unit/exampleUnitTest.test.ts
+++ src/test/unit/exampleUnitTest.test.ts
@@ -1,11 +1,6 @@
 import * as assert from 'assert';
-import * as fs from 'fs';
-import * as os from 'os';
-import * as path from 'path';
 
 suite('JsonResultParser Unit Tests', () => {
-    let projectRoot: string;
-    let fullResultPath: string;
 
     suiteSetup(() => {});
 
